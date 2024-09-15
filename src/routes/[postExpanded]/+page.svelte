e80d041a8e816c058ce0d64ce574c8a1333e51a1
Sergej Smelov <109071165+c0dehamster@users.noreply.github.com>
2024-09-16 00:33:06 +0500

    Stye the selected post

--- src/routes/[postExpanded]/+page.svelte
+++ src/routes/[postExpanded]/+page.svelte
@@ -2,6 +2,7 @@
     import { page } from "$app/stores"
     import { testPosts } from "$lib/test-data/testPost"
     import type { Post } from "$lib/types/post"
+    import Tags from "../Tags.svelte"
 
     let posts = testPosts
     let post: Post | undefined // Needs a proper fix
@@ -15,17 +16,31 @@
     <main class="main">
         {#if post}
             <div class="post">
-                {#if post.title}
-                    <h1 class="post__title">{post.title}</h1>
-                {/if}
+                <div class="post__contents">
+                    {#if post.title}
+                        <h1 class="post__title">{post.title}</h1>
+                    {/if}
 
-                {#if post.image}
-                    <img src={post.image} alt="" class="post__image" />
-                {/if}
+                    {#if post.image}
+                        <img src={post.image} alt="" class="post__image" />
+                    {/if}
 
-                {#if post.bodyText}
-                    <p class="post__body-text">{post.bodyText}</p>
-                {/if}
+                    {#if post.bodyText}
+                        <p class="post__body-text">{post.bodyText}</p>
+                    {/if}
+                </div>
+
+                <div class="tags">
+                    <p class="tags__label">Tags:</p>
+
+                    {#if post.tags}
+                        <ul class="tags__list">
+                            {#each post.tags as tag}
+                                <li class="tags__list-item">{tag}</li>
+                            {/each}
+                        </ul>
+                    {/if}
+                </div>
             </div>
         {/if}
     </main>
@@ -45,9 +60,88 @@
     .post {
         position: relative;
 
+        max-width: 64rem;
+
         display: flex;
         flex-direction: column;
 
         background-color: var(--color-neutral-800);
+    }
+
+    .post__contents {
+        padding: 2rem;
+
+        display: flex;
+        flex-direction: column;
+        gap: 1.5rem;
+    }
+
+    .post__title {
+        font-size: var(--font-size-600);
+    }
+
+    /* Tags */
+
+    .tags {
+        padding-inline: 2rem;
+        padding-block: 1rem;
+
+        display: flex;
+        align-items: center;
+        gap: 2rem;
+
+        background-color: var(--color-primary-400);
+    }
+
+    .tags__list {
+        display: flex;
+        align-items: center;
+        flex-wrap: wrap;
+        row-gap: 0.5rem;
+        column-gap: 1.5rem;
+    }
+
+    .tags__label {
+        font-weight: bold;
+    }
+
+    .tags__list-item {
+        color: var(--color-neutral-200);
+    }
+
+    @media screen and (width > 40rem) {
+        .post {
+            padding: 3rem;
+
+            gap: 2rem;
+        }
+
+        .post__contents {
+            padding: 0rem;
+        }
+
+        .post__title {
+            font-size: var(--font-size-700);
+        }
+
+        .post__body-text {
+            font-size: var(--font-size-500);
+        }
+
+        .tags {
+            font-size: var(--font-size-500);
+        }
+    }
+
+    @media screen and (width > 64rem) {
+        .post {
+            padding: 4rem;
+
+            gap: 2.5rem;
+        }
+
+        .post__contents {
+            gap: 2.5rem;
+        }
     }
 </style>
